d4312c509490320927d95c284e32c5293b34fa05
DavutOvez <dawut2010owezow@gmail.com>
2025-08-01 16:19:25 +0500

    update something

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from "@vercel/analytics/react"
+
 import {
   BrowserRouter as Router,
   useLocation,
